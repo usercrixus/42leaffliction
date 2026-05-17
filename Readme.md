dd886eee9549ddc8071015eaa547f70f651ac64b
Lilian <lilianperthuis@gmail.com>
2026-05-18 00:30:15 +0200

    fix(Augmentation): Show transformation and same in same folder than the image for single image call

--- Readme.md
+++ Readme.md
@@ -2,38 +2,62 @@
 make reset
 ```
 
+## Create virtual env
 ```bash
 make
 ```
 
+## Check norm
 ```bash
 make norm
 ```
 
+
+## Distribution
+
 ```bash
 make distribution
 ```
 
+## Augmentation
+
+on a single image:
+```bash
+python -m srcs.Augmentation.Augmentation [img_path]
+```
+
+on the whole dataset:
 ```bash
 make augmentation
 ```
 
+or for more precise instructions
+```bash
+python -m srcs.Augmentation.Transformation -d [on/off] -n 6 -s {outdir} {path_to_image}
+```
+
 ```bash
 make distribution
 ```
 
+then:
 ```bash
 make transformation
 ```
+
+## Training
 
 ```bash
 make train NAME_TAIL="_original" EPOCHS=1 BATCH_SIZE=16
 ```
 
+## Predict
+
 ```bash
 make predict IMG_STEM="data/images_transformed/Apple_Black_rot/image (1)" NAME_TAIL="_original"
 ```
 
+## Check dataset signature
 ```bash
 make signature
 ```
--- srcs/Augmentation/Augmentation.py
+++ srcs/Augmentation/Augmentation.py
@@ -1,8 +1,12 @@
 import sys
 import os
+import numpy as np
 from pathlib import Path
 from .Transformation import transform, save
 from ..Distribution.Distribution import get_data
+from PIL import Image
+import matplotlib.pyplot as plt
+
 
 
 def equilibrate_data(data, path):
@@ -21,6 +25,7 @@ def equilibrate_data(data, path):
             if img == len(imgs):
                 break
             img_path = os.path.join(new_path, imgs[img])
+            img_original = np.array(Image.open(img_path))
             t_imgs = []
             if best_v - values[i] >= 6:
                 t_imgs = [transform(img_path, j) for j in range(6)]
@@ -28,7 +33,8 @@ def equilibrate_data(data, path):
                 t_imgs = [
                     transform(img_path, j) for j in range(best_v - values[i])
                     ]
-            save(t_imgs, save_dir, img_path, False)
+            t_imgs = [img_original] + t_imgs
+            save(t_imgs, save_dir, img_path, True)
             values = list(get_data(path).values())
             if values[i] == best_v:
                 break
@@ -41,13 +47,37 @@ def equilibrate_data(data, path):
         equilibrate_data(get_data(path), path)
 
 
+def equilibrate_file(img_path: str, dir_path: str):
+
+    if not Path(dir_path).exists():
+        os.makedirs(dir_path)
+
+    save_dir = os.path.abspath(dir_path)
+
+    img_original = np.array(Image.open(img_path))
+
+    t_imgs = [transform(img_path, j) for j in range(6)]
+    t_imgs = [img_original] + t_imgs
+    save(t_imgs, save_dir, img_path, True)
+
+    fig, axes = plt.subplots(1, 7, figsize=(18, 4))
+    for ax, img in zip(axes, t_imgs):
+        ax.imshow(img)
+        ax.axis("off")
+    plt.tight_layout()
+    plt.show()
+
+
 def main():
     if len(sys.argv) != 2:
         print("Path folder is missing.")
         exit(1)
     pathname = Path(sys.argv[1])
-    data = get_data(pathname)
-    equilibrate_data(data, pathname)
+    if pathname.is_file():
+        equilibrate_file(pathname, pathname.parent)
+    elif pathname.is_dir():
+        data = get_data(pathname)
+        equilibrate_data(data, pathname)
 
 
 if __name__ == "__main__":
